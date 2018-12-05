86fa3924d875c1d96b18d29c1f67e4e8ea09c8ce
Maciej Derewiański <maciej.derewianski@onet.eu>
2018-12-05 10:37:14 +0100

    Update COntactForm.js

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -11,31 +11,6 @@ var ContactForm = React.createClass({
           <input type={'email'} placeholder={'email'} value={this.props.contact.email}>
           <button type={submit}>Dodaj kontakt</button>
         </form>
-
-
-
-      /* React.createElement('form', {className: 'contactForm'},
-        React.createElement('label', {for: 'name'}, "Imię: "),
-        React.createElement('input', {
-          type: 'text',
-          placeholder: 'Imię',
-          value: this.props.contact.firstName,
-        }),
-        React.createElement('label', {for: 'nazwisko'}, "Nazwisko: "),
-        React.createElement('input', {
-          type: 'text',
-          placeholder: 'Nazwisko',
-          value: this.props.contact.lastName,
-        }),
-        React.createElement('label', {for: 'email'}, "Email: "),
-        React.createElement('input', {
-          type: 'email',
-          placeholder: 'email',
-          value: this.props.contact.email,
-        }),
-        React.createElement('button', {type: 'submit'}, "Dodaj kontakt"),
-
-      ) */
     )
   },
 })
