09876a0bdbb78a1e6450eb2cb71805bed5194bf7
Maciej Derewiański <maciej.derewianski@onet.eu>
2018-12-05 11:26:07 +0100

    Update c

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -6,11 +6,11 @@ var ContactForm = React.createClass({
     return
         <form className={'contactForm'}>
           <label>Imię: </label>
-          <input type={'text'} placeholder={'Imię'} value={this.props.contact.firstName}>
+          <input type={'text'} placeholder={'Imię'} value={this.props.contact.firstName}/>
           <label>Nazwisko: </label>
-          <input type={'text'} placeholder={'Nazwisko'} value={this.props.contact.lastName}>
+          <input type={'text'} placeholder={'Nazwisko'} value={this.props.contact.lastName}/>
           <label>Email: </label>
-          <input type={'email'} placeholder={'email'} value={this.props.contact.email}>
+          <input type={'email'} placeholder={'email'} value={this.props.contact.email}/>
           <button type={'submit'}>Dodaj kontakt</button>
         </form>
   },
