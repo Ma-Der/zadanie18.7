606c150691f036e209a4972e21fb019274d2e42f
Maciej Derewiański <maciej.derewianski@onet.eu>
2018-12-05 10:27:28 +0100

    Update contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -5,7 +5,14 @@ var Contact = React.createClass({
 
   render: function() {
     return (
-      React.createElement('div', {className: 'contactItem'},
+        <div className={'contactItem'}>
+          <img className={'contactImage'} src={'https://via.placeholder.com/150'} />
+          <p className={'contactLabel'}>Imię: {this.props.item.firstName}</p>
+          <p className={'contactLabel'}>Nazwisko: {this.props.item.lastName}></p>
+          <a className={'contactEmail'} href={'mailto: ' + this.props.item.email}>{this.props.item.email}</a>
+
+
+      /* React.createElement('div', {className: 'contactItem'},
         React.createElement('img', {
           className: 'contactImage',
           src: 'https://via.placeholder.com/150'
@@ -13,7 +20,7 @@ var Contact = React.createClass({
         React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
         React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
         React.createElement('a', {className: 'contactEmail', href: 'mailto: ' + this.props.item.email}, this.props.item.email)
-      )
+      )  */
     )
   },
 });
