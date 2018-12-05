934a7c9b5da560377c8444ecc57ff497a5c1cd4d
Maciej Derewiański <maciej.derewianski@onet.eu>
2018-12-05 14:06:13 +0100

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -4,12 +4,13 @@ Contact = React.createClass({
   },
 
   render: function() {
-    return
+    return (
         <div className={'contactItem'}>
           <img className={'contactImage'} src={'https://via.placeholder.com/150'} />
           <p className={'contactLabel'}>Imię: {this.props.item.firstName}</p>
           <p className={'contactLabel'}>Nazwisko: {this.props.item.lastName}></p>
           <a className={'contactEmail'} href={'mailto: ' + this.props.item.email}>{this.props.item.email}</a>
         </div>
+      );
   },
 });
