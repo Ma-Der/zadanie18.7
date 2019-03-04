37c1e70c3c68e69e7dc12a5a9df6ccceb5bb9896
Maciej Derewiański <eco_derma@poczta.onet.pl>
2019-03-04 18:16:43 +0100

    Update

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,11 +1,13 @@
 ContactForm = React.createClass({
   getInitialState: function(){
     return {
-      contactForm: {
-        firstName: '',
-        lastName: '',
-        email: ''
-      }
+      contactForm: [
+        {
+          firstName: '',
+          lastName: '',
+          email: ''
+        }
+      ]
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -18,24 +20,30 @@ ContactForm = React.createClass({
 
   handleChange: function(e) {
     this.setState({
-      contactForm: e.target.value
+      contactForm: [
+        {
+          firstName: e.target.value,
+          lastName: e.target.value,
+          email: e.target.value
+        }
+      ]
     });
   },
 
   handleSubmit: function(e) {
     e.preventDefault();
     console.log(e);
-    this.props.onContactAdd(e.target.value);
+    this.props.onContactAdd(this.state.contactForm.firstName, this.state.contactForm.lastName, this.state.contactForm.email);
   },
   render: function() {
     return (
         <form className={'contactForm'} onSubmit={this.handleSubmit}>
           <label>Imię: </label>
-          <input type={'text'} placeholder={'Imię'} onChange={this.handleChange} />
+          <input type={'text'} placeholder={'Imię'} value={this.state.contactForm.firstName} onChange={this.handleChange} />
           <label>Nazwisko: </label>
-          <input type={'text'} placeholder={'Nazwisko'} onChange={this.handleChange}/>
+          <input type={'text'} placeholder={'Nazwisko'} value={this.state.contactForm.lastName} onChange={this.handleChange}/>
           <label>Email: </label>
-          <input type={'email'} placeholder={'email'} onChange={this.handleChange}/>
+          <input type={'email'} placeholder={'email'} value={this.state.contactForm.email} onChange={this.handleChange}/>
           <button type={'submit'}>Dodaj kontakt</button>
         </form>
       );
