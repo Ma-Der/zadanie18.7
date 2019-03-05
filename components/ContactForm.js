5006156c5589daa4461e6131619e14f4d1b3d35e
Maciej Derewiański <eco_derma@poczta.onet.pl>
2019-03-05 17:19:10 +0100

    Update

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,13 +1,9 @@
 ContactForm = React.createClass({
   getInitialState: function(){
     return {
-      contactForm: [
-        {
           firstName: '',
           lastName: '',
           email: ''
-        }
-      ]
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -19,31 +15,28 @@ ContactForm = React.createClass({
   },
 
   handleChange: function(e) {
+    console.log(e);
     this.setState({
-      contactForm: [
-        {
           firstName: e.target.value,
           lastName: e.target.value,
           email: e.target.value
-        }
-      ]
     });
   },
 
   handleSubmit: function(e) {
     e.preventDefault();
-    console.log(e);
-    this.props.onContactAdd(this.state.contactForm.firstName, this.state.contactForm.lastName, this.state.contactForm.email);
+    this.props.onContactAdd(this.state.firstName, this.state.lastName, this.state.email);
+    console.log(this.props.onContactAdd(this.state.firstName, this.state.lastName, this.state.email));
   },
   render: function() {
     return (
         <form className={'contactForm'} onSubmit={this.handleSubmit}>
           <label>Imię: </label>
-          <input type={'text'} placeholder={'Imię'} value={this.state.contactForm.firstName} onChange={this.handleChange} />
+          <input type={'text'} placeholder={'Imię'} name={this.state.firstName} onChange={this.handleChange} />
           <label>Nazwisko: </label>
-          <input type={'text'} placeholder={'Nazwisko'} value={this.state.contactForm.lastName} onChange={this.handleChange}/>
+          <input type={'text'} placeholder={'Nazwisko'} name={this.state.lastName} onChange={this.handleChange}/>
           <label>Email: </label>
-          <input type={'email'} placeholder={'email'} value={this.state.contactForm.email} onChange={this.handleChange}/>
+          <input type={'email'} placeholder={'email'} name={this.state.email} onChange={this.handleChange}/>
           <button type={'submit'}>Dodaj kontakt</button>
         </form>
       );
